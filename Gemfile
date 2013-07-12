46b7b42321af4631e3407dc710841ff67ac1bdd3
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-07-12 14:50:01 +0200

    Added microdata example

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'microformats2', :git => 'git://github.com/G5/microformats2.git'
+gem 'microdata'
 gem 'rspec'
 gem 'pry'
 
--- client_spec.rb
+++ client_spec.rb
@@ -2,11 +2,12 @@
 
 require "net/http"
 require "microformats2"
+require "microdata"
 require "pp"
 require "pry"
 
-describe "#foo" do
-  it "fails" do
+describe "microformat parsing" do
+  it "parses a kanban item" do
     response = Net::HTTP.get('localhost', '/items', 3000) 
     collection = Microformats2.parse(response)
     # binding.pry
@@ -15,5 +16,18 @@ describe "#foo" do
 
     item.title.to_s.should eq("It works here too")
     item.status.to_s.should eq("backlog")
+  end
+end
+
+describe "microdata parsing" do
+  it "parses a kanban item" do
+
+    items = Microdata.get_items('http://localhost:3000/items')
+   
+    # binding.pry
+    item = items.first.properties["column"].first.properties["item"].first
+
+    item.properties["title"].first.should eq("It works here too")
+    item.properties["status"].first.should eq("backlog")
   end
 end
