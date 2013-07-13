5310dd79bf671b1b28aa7355e17528eb9adc3f5f
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-07-13 23:45:02 +0200

    Indent

--- text_client.rb
+++ text_client.rb
@@ -2,46 +2,37 @@ require "mechanize"
 require "pry"
 
 loop do
-agent = Mechanize.new
-page = agent.get("http://localhost:3000/items")
+  agent = Mechanize.new
+  page = agent.get("http://localhost:3000/items")
 
-items_node = page.search("[itemtype$=KanbanItem]")
-items = items_node.map.with_index do |item, index| 
-  forms = item.search("form.move").map do |form| 
+  items_node = page.search("[itemtype$=KanbanItem]")
+  items = items_node.map.with_index do |item, index| 
+    forms = item.search("form.move").map do |form| 
+      {
+        :name => form.attribute("class").value.split(" ")[1],
+        :form => Mechanize::Form.new(form, agent, page) 
+      }
+    end
+
     {
-      :name => form.attribute("class").value.split(" ")[1],
-      :form => Mechanize::Form.new(form, agent, page) 
+      :title => item.search("[itemprop=title]").text,
+      :description => item.search("[itemprop=status]").text,
+      :index => index,
+      :forms => forms 
     }
   end
 
-  {
-    :title => item.search("[itemprop=title]").text,
-    :description => item.search("[itemprop=status]").text,
-    :index => index,
-    :forms => forms 
-  }
-end
-
-# pp items
-
-# items[0][:forms].find {|form| form[:name] == "working"}[:form].submit
-
-items.each do |item|
-  puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
-  item[:forms].each do |form|
-    puts "  " + form[:name]
+  items.each do |item|
+    puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
+    item[:forms].each do |form|
+      puts "  " + form[:name]
+    end
+    puts
   end
-  puts
-end
 
-puts "Enter command: "
-input = gets.chomp
-args = input.split(" ")
-# binding.pry
-items[args[0].to_i][:forms].find {|form| !!form[:name].match(/\A#{args[1]}/)}[:form].submit
+  puts "Enter command: "
+  input = gets.chomp
+  args = input.split(" ")
 
-
-#binding.pry
-
-#pp items.first[:forms]
+  items[args[0].to_i][:forms].find {|form| !!form[:name].match(/\A#{args[1]}/)}[:form].submit
 end
