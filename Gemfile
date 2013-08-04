2429d680fe750d8dab3536f1df256cfed0fbb3e3
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-08-04 14:25:54 +0200

    Cleanup tests and deps

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'microformats2', :git => 'git://github.com/G5/microformats2.git'
-gem 'microdata'
 gem 'rspec'
 gem 'pry'
 gem 'mechanize'
--- client_spec.rb
+++ client_spec.rb
@@ -1,11 +1,7 @@
 
-
 require "net/http"
-require "microformats2"
-require "microdata"
 require "pp"
 require "pry"
-require "nokogiri"
 require 'open-uri'
 require 'mechanize'
 
@@ -20,55 +16,8 @@ describe "mechanize" do
     form.field_with(:name => "item[description]").value = "Description from test"
     # form.submit
 
-    binding.pry
+    # binding.pry
   end
 end
 
-describe "microformat parsing" do
-  it "parses a kanban item" do
-    response = Net::HTTP.get('localhost', '/items', 3000) 
-    collection = Microformats2.parse(response)
-    # binding.pry
 
-    item = collection.first
-
-    item.title.to_s.should eq("It works here too")
-    item.status.to_s.should eq("backlog")
-  end
-end
-
-describe "microdata parsing" do
-  it "parses a kanban item" do
-
-    items = Microdata.get_items('http://localhost:3000/items')
-   
-    # binding.pry
-    item = items.first.properties["column"].first.properties["item"].first
-
-    item.properties["title"].first.should eq("It works here too")
-    item.properties["status"].first.should eq("backlog")
-  end
-end
-
-describe "nokogiri parsing" do
-
-  before(:each) do
-    @doc = Nokogiri::HTML(open("http://localhost:3000/items")) 
-    # binding.pry
-  end
-
-  it "parses the title " do
-    @doc.at_css("h1").text.should eq("My kanban board")
-  end
-
-  it "parses a kanban item" do
-    item = @doc.at_css("[itemprop=item]")
-    item.css("[itemprop=title]").text.should eq("It works here too")
-  end
-  
-  it "has a link to the create form for a new item" do 
-    # binding.pry
-    link = @doc.css("[itemtype$=KanbanBoard] a[rel=create]").first['href']
-    link.should eq("/items/new")
-  end
-end
