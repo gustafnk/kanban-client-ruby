c67f332885847e2c18fade0228b959b98c1a0447
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-09-01 19:55:30 +0200

    Add categories

--- text_client.rb
+++ text_client.rb
@@ -1,40 +1,72 @@
 require "mechanize"
 require "pry"
 
-loop do
-  agent = Mechanize.new
-  page = agent.get("http://localhost:3000/items")
+def parse_item (item_node, index)
 
-  categories_node = page.search(".h-column")
-  # binding.pry
-  items_node = page.search(".h-item")
-  items = items_node.map.with_index do |item, index| 
-    forms = item.search("form.move").map do |form| 
-      {
-        :name => form.attribute("class").value.split(" ")[1],
-        :form => Mechanize::Form.new(form, agent, page) 
-      }
-    end
-
+  forms = item_node.search("form.move").map do |form| 
     {
-      :title => item.search(".p-title").text,
-      :description => item.search(".p-description").text,
-      :index => index,
-      :forms => forms 
+      :name => form.attribute("class").value.split(" ")[1],
+      :form => Mechanize::Form.new(form, @agent, @page) 
     }
   end
 
-  items.each do |item|
-    puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
-    item[:forms].each do |form|
-      puts "  " + form[:name]
+  {
+    :title => item_node.search(".p-title").text,
+    :description => item_node.search(".p-description").text,
+    :index => index,
+    :forms => forms 
+  }
+end
+
+loop do
+
+  @agent = Mechanize.new
+  @page = @agent.get("http://localhost:3000/items")
+
+  categories_node = @page.search(".h-column")
+  categories = categories_node.map do |category_node|
+
+    item_nodes = category_node.search(".h-item")
+    items = item_nodes.map.with_index do |item, index|
+      parse_item(item, index)
     end
-    puts
+
+    { 
+      :name => category_node.search(".p-name").text,
+      :items => items
+    }
+
   end
 
-  puts "Enter command: "
+  items_node = @page.search(".h-item")
+  items = items_node.map.with_index do |item, index| 
+    parse_item(item, index)
+  end
+
+  categories.each do |category|
+    puts "------------------------"
+    puts category[:name]
+    puts "------------------------"
+    category[:items].each do |item|
+      puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
+      item[:forms].each do |form|
+        puts "  " + form[:name]
+      end
+      puts
+    end
+  end
+
+  puts "Enter command: (from index to)"
   input = gets.chomp
   args = input.split(" ")
 
-  items[args[0].to_i][:forms].find {|form| !!form[:name].match(/\A#{args[1]}/)}[:form].submit
+  found_category = categories.select do |category| 
+    category[:name].match /#{args[0]}/i 
+  end
+
+  form = found_category[0][:items][args[1].to_i][:forms].find do |form| 
+    !!form[:name].match(/\A#{args[2]}/)
+  end
+
+  form[:form].submit
 end
