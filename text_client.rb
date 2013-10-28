ed8a5b18ce5fe59fe2c6c017aaa8729c0c2d2daa
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-10-28 16:02:59 +0100

    Extract functions

--- text_client.rb
+++ text_client.rb
@@ -1,7 +1,75 @@
 require "mechanize"
 require "pry"
 
-def parse_item (item_node, index)
+##### I/O functions
+
+def display_category(category)
+  puts "------------------------"
+  puts category[:name]
+  puts "------------------------"
+
+  category[:items].each do |item|
+    # TODO Use string interpolation
+    puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
+
+    item[:forms].each do |form|
+      if (form[:is_next])
+        puts " >" + form[:name]
+      else
+        puts "  " + form[:name]
+      end
+    end
+    puts
+  end
+end
+
+def get_form_from_input(categories)
+  input = gets.chomp
+
+  if (input.empty?)
+    return nil
+  end
+
+  args = input.split(" ")
+
+  found_category = categories.select do |category| 
+    category[:name].match /^#{args[0]}/i 
+  end
+
+  form = found_category.first()[:items][args[1].to_i][:forms].find do |form| 
+    if !!args[2]
+      !!form[:name].match(/\A#{args[2]}/)
+    else
+      form[:is_next]
+    end
+  end
+
+  form
+end
+
+def display_prompt
+  puts
+  puts "Enter command: (from index [to])"
+end
+
+
+##### Extracting functions
+
+def extract_category(category_node)
+
+  item_nodes = category_node.search(".h-item")
+
+  items = item_nodes.map.with_index do |item, index|
+    extract_item(item, index)
+  end
+
+  { 
+    :name => category_node.search("> .p-name").text,
+    :items => items
+  }
+end
+
+def extract_item (item_node, index)
 
   forms = item_node.search(".p-forms form").map do |form| 
     {
@@ -19,70 +87,31 @@ def parse_item (item_node, index)
   }
 end
 
+
+##### Main loop
+
 loop do
   system("clear") 
 
   @agent = Mechanize.new
   @page = @agent.get("http://localhost:3001/items")
 
-  categories_node = @page.search(".h-column")
-  categories = categories_node.map do |category_node|
-
-    item_nodes = category_node.search(".h-item")
-
-    items = item_nodes.map.with_index do |item, index|
-      parse_item(item, index)
-    end
-
-    { 
-      :name => category_node.search("> .p-name").text,
-      :items => items
-    }
-
+  category_nodes = @page.search(".h-board .h-column")
+  categories = category_nodes.map do |category_node|
+    extract_category(category_node)
   end
 
   categories.each do |category|
-    # TODO Extract display_category
-    puts "------------------------"
-    puts category[:name]
-    puts "------------------------"
-
-    # TODO Extract display_items
-    category[:items].each do |item|
-      puts "(" + item[:index].to_s + ") " + item[:title] + ", " + item[:description]
-
-      item[:forms].each do |form|
-        if (form[:is_next])
-          puts " >" + form[:name]
-        else
-          puts "  " + form[:name]
-        end
-      end
-      puts
-    end
+    display_category(category)
   end
 
-  puts
-  puts "Enter command: (from index [to])"
+  display_prompt()
 
-  # TODO Extract get_form_from_input
-  input = gets.chomp
+  form = get_form_from_input(categories)
 
-  args = input.split(" ")
-
-  found_category = categories.select do |category| 
-    category[:name].match /^#{args[0]}/i 
+  if (!form)
+    next
   end
-
-  form = found_category[0][:items][args[1].to_i][:forms].find do |form| 
-    if !!args[2]
-      !!form[:name].match(/\A#{args[2]}/)
-    else
-      form[:is_next]
-    end
-  end
-
-  # TODO End extract get_form_from_input
 
   form[:form].submit
 end
